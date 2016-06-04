a565104202db380c5475d76340b1034a1a7efaa5
Oleksii Orel <oorel@codenvy.com>
2016-06-04 18:06:36 +0300

    remove unused factory
    Signed-off-by: Oleksii Orel <oorel@codenvy.com>

--- dashboard/src/app/navbar/navbar.controller.js
+++ dashboard/src/app/navbar/navbar.controller.js
@@ -16,13 +16,12 @@ export class CheNavBarCtrl {
    * Default constructor
    * @ngInject for Dependency injection
    */
-  constructor($mdSidenav, $scope, $location, $route, userDashboardConfig, cheAPI, onBoarding) {
+  constructor($mdSidenav, $scope, $location, $route, userDashboardConfig, cheAPI) {
     this.mdSidenav = $mdSidenav;
     this.$scope = $scope;
     this.$location = $location;
     this.$route = $route;
     this.cheAPI = cheAPI;
-    this.onBoarding = onBoarding;
     this.cheUser = cheAPI.getUser();
     this.links = [{href: '#/create-workspace', name: 'New Workspace'}];
 
